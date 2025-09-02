f356c7758394c14a827510b921251fc3539dfbf5
sultangithub04 <sult_04@yahoo.com>
2025-09-02 07:24:54 +0600

    some updated

--- src/utils/LoadingSpinner.tsx
+++ src/utils/LoadingSpinner.tsx
@@ -23,3 +23,28 @@ const LoadingSpinner: FC<LoadingSpinnerProps> = ({ smallHeight = false }) => {
 }
 
 export default LoadingSpinner
+
+import type { FC } from 'react'
+import { ScaleLoader } from 'react-spinners'
+
+interface LoadingSpinnerProps {
+  smallHeight?: boolean
+}
+
+const LoadingSpinner: FC<LoadingSpinnerProps> = ({ smallHeight = false }) => {
+  return (
+    <div
+      className={`
+        ${smallHeight ? 'h-[250px]' : 'h-[70vh]'}
+        flex
+        flex-col
+        justify-center
+        items-center
+      `}
+    >
+      <ScaleLoader  color="red" />
+    </div>
+  )
+}
+
+export default LoadingSpinner
